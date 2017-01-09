29f7d9f6131894a71609ed6b0efff8124c829580
Noel Paredes <ing.noelparedes@gmail.com>
2017-01-09 03:36:13 -0400

    Agregando AICRDWEB.Data que Falto

---
+++ AICRDWEB.Data/Circuitos.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AICRDWEB.Models
+{
+    [Table("Circuitos")]
+    public class Circuitos
+    {
+        [Display(Name = "Circuito")]
+        public int IdCircuito { get; set; }
+
+        [Display(Name = "Region")]
+        public int IdRegion { get; set; }
+
+
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(50, ErrorMessage = "La longitud máxima del campo {0} es {1}")]
+        public string NombreCircuito { get; set; }
+
+
+        [StringLength(100, ErrorMessage = "La longitud máxima del campo {0} es {1}")]
+        public string Descripcion { get; set; }
+
+        [ForeignKey("IdRegion")]
+        public virtual Region Region { get; set; }
+
+
+
+    }
+}
---
+++ AICRDWEB.Data/Iglesias.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AICRDWEB.Models
+{
+
+    [Table("Iglesias")]
+    public class Iglesias
+    {
+        [Display(Name = "Iglesia")]
+        public int IdIglesia { get; set; }
+
+        [Display(Name = "Circuito")]
+        public int IdCircuito { get; set; }
+
+
+        [Display(Name = "Iglesia")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        [StringLength(30, ErrorMessage = "La longitud máxima del campo {0} es {1}")]
+        public string NombreIglesia { get; set; }
+
+        [Display(Name = "Descripcion")]
+        [StringLength(150, ErrorMessage = "La longitud máxima del campo {0} es {1}")]
+        public string Descripcion { get; set; }
+
+        [Display(Name = "Cantidad de Miembros")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public int CantidadMiembro { get; set; }
+
+        [Display(Name = "Direccion")]
+        [Required(ErrorMessage = "El campo {0} es requerido")]
+        public string Direccion { get; set; }
+
+
+        //[Display(Name = "Direccion")]
+        //[Required(ErrorMessage = "El campo {0} es requerido")]
+        //public string Direccion { get; set; }
+
+        [ForeignKey("IdCircuito")]
+        public virtual Circuitos Circuito { get; set; }
+
+
+    }
+}
